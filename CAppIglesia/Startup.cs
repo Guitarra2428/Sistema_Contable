7f52a8223f673db5a007602f8b3ce57524321ebd
HogarDesarrolloHDN <113710297+HogarDesarrolloHDN@users.noreply.github.com>
2023-12-04 10:27:54 -0400

    Dada en memoria

--- CAppIglesia/Startup.cs
+++ CAppIglesia/Startup.cs
@@ -30,9 +30,13 @@ namespace CAppIglesia
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+
+            //Permite usar data en memoria 
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase(databaseName: "IglesiaDataMemory"));
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             
